4bc3f4424f36924111d90752dc54ac24cc945f0c
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-03 14:50:24 +0000

    Separated Header into a different component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './Navbar'
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
     <div>
       <Navbar />
-      <header className="masthead bg-image">
-        <div className="overlay"></div>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-md-10 mx-auto">
-              <div className="site-heading">
-                <h1>Clean Blog</h1>
-                <span className="subheading">A Blog Theme by Start Bootstrap</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <div className="container">
         <div className="row">
