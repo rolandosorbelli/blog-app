1c04b8c5ad953caba95024884cebd1e01a882b4e
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-11-03 14:57:40 +0000

    Separated Footer into a different component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Navbar from './Navbar'
 import Header from './Header'
 import Container from './Container'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
@@ -14,44 +15,9 @@ class App extends Component {
       <Header />
       <Container />
       <hr />
-
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-md-10 mx-auto">
-              <ul className="list-inline text-center">
-                <li className="list-inline-item">
-                  <a href="#">
-                    <span className="fa-stack fa-lg">
-                      <i className="fa fa-circle fa-stack-2x"></i>
-                      <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <span className="fa-stack fa-lg">
-                      <i className="fa fa-circle fa-stack-2x"></i>
-                      <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <span className="fa-stack fa-lg">
-                      <i className="fa fa-circle fa-stack-2x"></i>
-                      <i className="fa fa-github fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-              <p className="copyright text-muted">Copyright &copy; Your Website 2017</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />  
     </div>
-      );
+    );
   }
 }
 
